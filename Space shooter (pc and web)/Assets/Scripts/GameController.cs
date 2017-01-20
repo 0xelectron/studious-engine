38ee7db526f57fea4d131f32cc68d7f196b25eff
0xelectron <0xproton@gmail.com>
2017-01-20 13:25:16 +0530

    Added CS50 final project files

---
+++ Space shooter (pc and web)/Assets/Scripts/GameController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 SpawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText highscoreText;
+	public GUIText bestscoreText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	int highscore = 0;
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+		restart = false;
+		highscore = PlayerPrefs.GetInt("High Score");
+		gameOverText.text = "";
+		restartText.text =  ""; 
+		highscoreText.text = "";
+		bestscoreText.text = "Best Score: " + highscore;
+		score = 0;
+		UpdateScore();
+		StartCoroutine( SpawnWaves ());
+	}
+
+	void Update(){
+		if(score > highscore && gameOver ) //when player dies set highscore = to that score
+		{
+			highscore = score;
+			PlayerPrefs.SetInt("High Score", highscore);
+			PlayerPrefs.Save();
+			highscoreText.text = "New High Score: " + highscore;
+		}    
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator  SpawnWaves () {
+		yield return new  WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnposition = new Vector3 (Random.Range (-SpawnValues.x, SpawnValues.x), SpawnValues.y, SpawnValues.z);
+				Quaternion spawnrotation = Quaternion.identity;
+				Instantiate (hazard, spawnposition, spawnrotation);
+				yield return new  WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+			
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int NewScoreValue){
+		score += NewScoreValue;
+		UpdateScore ();
+		
+	}
+
+	void UpdateScore(){
+		scoreText.text = "Score: " + score;
+		}
+	public void GameOver(){
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
---
+++ space shooter (android)/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundry
+{
+	public float xMin,xMax,zMin,zMax;
+}
+
+
+public class PlayerControler : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundry boundry;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update()
+	{
+		if(Input.GetButton("Fire1") && Time .time > nextFire ) 
+		{
+			nextFire = Time .time + fireRate ;	
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+		}
+		Vector3 dir = Vector3.zero;
+		dir.x = Input.acceleration.x;
+		dir.z = Input.acceleration.y; 
+
+		if (dir.sqrMagnitude > 1)
+			
+			dir.Normalize();  
+		
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		
+		dir *= Time.deltaTime;
+		
+		// Move object
+		transform.Translate (dir * speed);
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody.position = new Vector3 
+			(
+				Mathf.Clamp (rigidbody.position.x , boundry.xMin , boundry.xMax ),
+				0.0f , 
+				Mathf.Clamp (rigidbody.position.z , boundry.zMin , boundry.zMax)
+			); 
+	
+		rigidbody.rotation = Quaternion.Euler (0.0f , rigidbody.velocity.x * - tilt ,0.0f );
+	}
+}
