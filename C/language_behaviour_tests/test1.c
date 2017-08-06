09765a6803e325a926ac63c98d97cb5b087fec6f
0xelectron <0xproton@gmail.com>
2017-08-06 10:38:37 +0530

    during sutdy of os161

---
+++ C/language_behaviour_tests/test1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/*
+* main: test weather value of statement is passed
+*/
+int
+main ()
+{
+    int a;
+    if ((a = 4) == 4) {
+        printf("Yep! It passess the value of the statement\n");
+    }
+}
